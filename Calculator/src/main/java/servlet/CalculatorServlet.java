7c92c2c2f41a1472df6c86833aa35028ad53c952
Harishma Arivazhagan <29046089+HarishmaA@users.noreply.github.com>
2018-08-05 20:03:10 +0530

    Add files via upload

---
+++ Calculator/src/main/java/servlet/CalculatorServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import javax.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CalculatorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		int firstNumber = Integer.parseInt(request.getParameter("FirstNumber"));
+		int secondNumber = Integer.parseInt(request.getParameter("SecondNumber"));
+		response.setContentType("text/html");
+		String op = request.getParameter("op");
+		PrintWriter out = response.getWriter();
+
+		if (op.equals("Addition")) {
+			out.println("<h4>The result is  </h4>"+ (firstNumber + secondNumber));
+		} else if (op.equals("Subtraction")) {
+			out.println("<h4>The result is  </h4>"+ (firstNumber - secondNumber));
+		} else if (op.equals("Multiplication")) {
+			out.println("<h4>The result is  </h4>"+ (firstNumber * secondNumber));
+		} else {
+			out.println("<h4>The result is  </h4>"+ (firstNumber / secondNumber));
+		}
+
+	}
+}
