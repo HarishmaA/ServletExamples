eeb4769a659b26b665911e5361762362a6047225
Harishma Arivazhagan <29046089+HarishmaA@users.noreply.github.com>
2018-08-23 18:50:07 +0530

    Update resultPrinter.js

--- EmployeeDetailsWithjQuery/src/main/webapp/resultPrinter.js
+++ EmployeeDetailsWithjQuery/src/main/webapp/resultPrinter.js
@@ -1,9 +1,16 @@
 function printEmployeeDetails(str) {
 	// First empty the <div> completely and add a title.
 	$("#employeeDetails-results").empty().append(
-			"<h1>EMPLOYEE DETAILS:</h1><br>").append(
-			"<h2>" + JSON.stringify(str) + "</h2>");
+			"<h1>EMPLOYEE DETAILS:</h1><br>");
+	$.each(str, function(key, value) {
+		$("#employeeDetails-results").append((key.toUpperCase()),"     ---     "+ value+"</br>");
+	});
+
 };
+
+// $("#employeeDetails-results").empty().append(
+// 			"<h1>EMPLOYEE DETAILS:</h1><br>").append(
+// 			"<h2>" + JSON.stringify(str) + "</h2>");
 
 
 
