047623c246655dfb6a3a148fbf488809ed493cdc
Harishma Arivazhagan <29046089+HarishmaA@users.noreply.github.com>
2018-08-26 17:54:35 +0530

    Update EmployeeDatabase.java

--- SubmitAndRetrieveEmployeesDetails/src/main/java/database/EmployeeDatabase.java
+++ SubmitAndRetrieveEmployeesDetails/src/main/java/database/EmployeeDatabase.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class EmployeeDatabase {
-	Map<Integer, Employee> records;
+	private Map<Integer, Employee> records;
 
 	public EmployeeDatabase() {
 		records = new HashMap<>();
