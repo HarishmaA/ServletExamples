bbfb55e0fda4803448c3e359a7a201ebf72ff28b
Harishma Arivazhagan <29046089+HarishmaA@users.noreply.github.com>
2018-08-26 15:30:07 +0530

    Add files via upload

---
+++ SubmitAndRetrieveEmployeesDetails/src/main/java/database/EmployeeDatabase.java
@@ -0,0 +1,35 @@
+package database;
+
+import model.Employee;
+import java.util.Map;
+import java.util.HashMap;
+
+public class EmployeeDatabase {
+	Map<Integer, Employee> records;
+
+	public EmployeeDatabase() {
+		records = new HashMap<>();
+	}
+
+	// Singleton methods
+	private static EmployeeDatabase dbInstance = null;
+
+	public static EmployeeDatabase getInstance() {
+		if (dbInstance == null) {
+			dbInstance = new EmployeeDatabase();
+		}
+		return dbInstance;
+	}
+
+	public void addDetails(Integer id, Employee employee) {
+
+		records.put(id, employee);
+	}
+
+	public Employee getEmployeeDetails(Integer input) {
+		if (!records.containsKey(input)) {
+			return null;
+		}
+		return records.get(input);
+	}
+}
---
+++ SubmitAndRetrieveEmployeesDetails/src/main/webapp/validateAndRequestSender.js
@@ -0,0 +1,27 @@
+// When the page is fully loaded...
+$(document).ready(function() {
+	$("#identityNumber").click(function(event) {
+		var identity = $("#id").val();
+		if (identity === "") {
+			$(".error-messages").text("INPUT FIELD IS EMPTY!!!").fadeIn();
+			return false;
+		}
+		if (isNaN(identity)) {
+			$(".error-messages").text("NOT A VALID NUMBER!!!").fadeIn();
+			return false;
+		}
+		if (!((Number(identity)>=1) && (Number(identity)<=10))) {
+			$(".error-messages").text("ENTER A NUMBER FROM 1 TO 10!!!").fadeIn();
+			return false;
+		}
+		$(".error-messages").empty().fadeOut();
+		$.get('/details', {
+			"identity" : identity
+		}, function(data) { // on success
+			printEmployeeDetails(data);
+		},"json").fail(function(data) { // on failure
+			printEmployeeDetails(data);
+			$(".error-messages").html("<h3>REQUEST FAILED!!!</h3>").fadeIn();;
+		});
+	});
+});
