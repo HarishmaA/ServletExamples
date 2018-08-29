265b562d8b4b3e8f0a11411776124b69816ffd1d
Harishma Arivazhagan <29046089+HarishmaA@users.noreply.github.com>
2018-08-29 10:32:59 +0530

    Add files via upload

---
+++ SubmitAndRetrieveEmployeesDetails/src/main/java/model/Employee.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Employee {
+	private int employeeId;
+	private String employeeName;
+	private int employeeAge;
+	private String employeeDesignation;
+	private String teamName;
+	private double employeeSalary;
+
+	// Constructor
+	public Employee() {
+	}
+
+	public Employee(int employeeId, String employeeName, int employeeAge, String employeeDesignation, String teamName,
+			double employeeSalary) {
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeAge = employeeAge;
+		this.employeeDesignation = employeeDesignation;
+		this.teamName = teamName;
+		this.employeeSalary = employeeSalary;
+	}
+
+}
